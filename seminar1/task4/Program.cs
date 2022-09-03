40f1c77576520407ca240e903e9f7666bab8dbc6
dzoric1 <Gerasimov2099@mail.ru>
2022-09-03 16:22:53 +0800

    realized task4

---
+++ seminar1/task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число:");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Наибольшее число - " + Math.Max(numberA, Math.Max(numberB, numberC)));
