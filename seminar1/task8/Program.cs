eed568bc6fd912473dbdb8224e9a6fa11ade35d6
dzoric1 <Gerasimov2099@mail.ru>
2022-09-03 16:51:33 +0800

    realized task8

---
+++ seminar1/task8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Находим четные числа от 1 до ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+
+if (number < 2)
+{
+   Console.WriteLine("Четных чисел в этом интервале нет");
+}
+
+while (i <= number)
+{
+   Console.Write(i + " ");
+   i += 2;
+}
