363a5d1f45b49b1082a7e50d3e9a831c19c9016c
dzoric1 <Gerasimov2099@mail.ru>
2022-09-12 15:16:46 +0800

    realized task 23

---
+++ seminar3/task23/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+   int i = 1;
+
+   while (number >= i)
+   {
+      System.Console.Write(Math.Pow(i, 3) + " ");
+      i++;
+   }
+}
+else
+{
+   System.Console.WriteLine("Введите число больше нуля");
+}
