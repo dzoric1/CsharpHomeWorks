26143d5634943ae36e839aec4409e5adf32cc3fa
dzoric1 <Gerasimov2099@mail.ru>
2022-09-12 20:41:49 +0800

    realized task 29

---
+++ seminar4/task29/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+   System.Console.Write($"Введите {i + 1} элемент массива:");
+   array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+System.Console.Write("Полученный массив: ");
+
+foreach (int arr in array)
+{
+
+   Console.Write(arr + " ");
+}
