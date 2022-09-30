617bac332785a1aa9db048e5cbfc280a17bccde6
dzoric1 <Gerasimov2099@mail.ru>
2022-09-30 18:33:27 +0800

    realized task66

---
+++ seminar9/task66/Program.cs
@@ -0,0 +1,29 @@
+﻿int ReadNumber(string message)
+{
+   Console.WriteLine(message);
+   return Convert.ToInt32(Console.ReadLine());
+}
+int SumMtoN(int m, int n)
+{
+   int sum = 0;
+   if (n < m)
+   {
+      for (int i = n; i <= m; i++)
+      {
+         sum += i;
+      }
+   }
+   else if (n > m)
+   {
+      for (int i = m; i <= n; i++)
+      {
+         sum += i;
+      }
+   }
+   else if (n == m) sum = n;
+   return sum;
+}
+
+int M = ReadNumber("Введите M");
+int N = ReadNumber("Введите N");
+System.Console.WriteLine($"Сумма чисел в промежутке от {M} до {N} = " + SumMtoN(M, N));
