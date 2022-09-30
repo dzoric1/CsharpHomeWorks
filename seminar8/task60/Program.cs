b54f7473819641078ac0c02af6de5ac6e2491460
dzoric1 <Gerasimov2099@mail.ru>
2022-09-30 16:21:03 +0800

    realized task60

---
+++ seminar8/task60/Program.cs
@@ -0,0 +1,20 @@
+﻿int[,,] array3D = new int[2, 2, 2] { { { 1, 2}, { 4, 5} },
+                                    { { 7, 8,}, { 10, 11} } };
+
+void PrintMatrix(int[,,] matrix)
+{
+   for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+      System.Console.WriteLine($"Глубина: {i + 1}");
+      for (int j = 0; j < matrix.GetLength(1); j++)
+      {
+         for (int k = 0; k < matrix.GetLength(1); k++)
+         {
+            Console.Write($"{matrix[i, j, k]}:({i},{j},{k})  ");
+         }
+         Console.WriteLine();
+      }
+   }
+}
+
+PrintMatrix(array3D);
