70abde08eb8fb4f44196d7efb4c0047e776181ba
dzoric1 <Gerasimov2099@mail.ru>
2022-09-30 18:09:24 +0800

    realized task64

---
+++ seminar9/task64/Program.cs
@@ -0,0 +1,16 @@
+﻿int ReadNumber(string message)
+{
+   Console.WriteLine(message);
+   return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNto1(int n)
+{
+   for (int i = n; i > 0; i--)
+   {
+      System.Console.Write(i + " ");
+   }
+}
+
+int number = ReadNumber("Введите N");
+PrintNto1(number);
