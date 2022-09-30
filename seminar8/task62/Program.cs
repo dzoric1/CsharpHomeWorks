75e9fe5eb0915aa3619a6a472e97d5e1461372f8
dzoric1 <Gerasimov2099@mail.ru>
2022-09-30 16:55:29 +0800

    realized task62

---
+++ seminar8/task62/Program.cs
@@ -0,0 +1,38 @@
+﻿int[,] SpiralMatrix(int[,] matrix)
+{
+   int temp = 1;
+   int i = 0;
+   int j = 0;
+
+   while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+   {
+      matrix[i, j] = temp;
+      temp++;
+      if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+         j++;
+      else if (i < j && i + j >= matrix.GetLength(0) - 1)
+         i++;
+      else if (i >= j && i + j > matrix.GetLength(1) - 1)
+         j--;
+      else
+         i--;
+   }
+   return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+   for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+      for (int j = 0; j < matrix.GetLength(1); j++)
+      {
+         Console.Write($"{matrix[i, j]}  ");
+      }
+      Console.WriteLine();
+   }
+}
+
+int[,] matr = new int[4, 4];
+SpiralMatrix(matr);
+PrintMatrix(matr);
